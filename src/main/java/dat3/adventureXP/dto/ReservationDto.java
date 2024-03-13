ec3fde9d5020c6a8d00ad22cc957ce908c1bc313
jack <jackvwh@hotmail.com>
2024-03-13 11:50:09 +0100

    fixed types in ReservationDto

--- src/main/java/dat3/adventureXP/dto/ReservationDto.java
+++ src/main/java/dat3/adventureXP/dto/ReservationDto.java
@@ -5,8 +5,8 @@ import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Set;
 
-import dat3.adventureXP.entity.Activity;
 import dat3.adventureXP.entity.Reservation;
+import dat3.adventureXP.entity.ReservationActivity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,13 +16,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ReservationDto {
      private Long id;
-     private GuestDto guest;  // Use GuestDto instead of Guest
+     private GuestDto guest;
      private Date reservationDate;
      private Time reservationTime;
      private Integer numberOfParticipants;
      private LocalDateTime created;
      private LocalDateTime edited;
-     private Set<Activity> activities;
+     private Set<ReservationActivity> activities;
 
 
      public ReservationDto(Reservation r ) {
@@ -32,6 +32,8 @@ public class ReservationDto {
          this.numberOfParticipants = r.getNumberOfParticipants();
          this.created = r.getCreated();
          this.edited = r.getEdited();
+         this.guest = new GuestDto(r.getGuest());
+         this.activities = r.getActivities();
      }
 
     public Long getGuestId() {
