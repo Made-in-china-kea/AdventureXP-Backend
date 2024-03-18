332f9f3137f9bec5906dda8866b493b0760f8f3e
Abed Rass <abd.assoum@outlook.com>
2024-03-18 14:38:50 +0100

    Created a test for activity entity

---
+++ src/test/java/dat3/adventureXP/entity/ActivityTest.java
@@ -0,0 +1,53 @@
+package dat3.adventureXP.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityTest {
+
+    @Test
+    void getId() {
+        
+    }
+
+    @Test
+    void testGetId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getAgeLimit() {
+    }
+
+    @Test
+    void getTimeSlot() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void setAgeLimit() {
+    }
+
+    @Test
+    void setTimeSlot() {
+    }
+}
