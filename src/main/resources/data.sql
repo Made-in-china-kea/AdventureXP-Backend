bfaa862ed88702352adcdbb4d544d6aad7f4b77c
jack <jackvwh@hotmail.com>
2024-03-18 10:47:19 +0100

    Add initial Activities data to database
    - Added INSERT statements for activities including gokart, sumowrestling, biking, mini-golf, and paintball with respective details
    - Noted that timeSlot can be null for biking activity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Activities (name, price, ageLimit, timeSlot) VALUES ('gokart', 50, 15, 100);
+INSERT INTO Activities (name, price, ageLimit, timeSlot) VALUES ('sumowrestling', 30, 18, 100);
+INSERT INTO Activities (name, price, ageLimit, timeSlot) VALUES ('biking', 20, 10, 300);  -- timeSlot can be null for biking
+INSERT INTO Activities (name, price, ageLimit, timeSlot) VALUES ('mini-golf', 15, 8, 200);
+INSERT INTO Activities (name, price, ageLimit, timeSlot) VALUES ('paintball', 40, 18, 200);
